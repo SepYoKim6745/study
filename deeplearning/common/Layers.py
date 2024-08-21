aaf561a863d0e1d0e3679452243e2e86d1ec9ea5
김요셉 <kim-yosep@gim-yoseb-ui-MacBookPro.local>
2024-08-21 21:43:24 +0900

    딥러닝 : TwoLayerNet.py 수정중

--- deeplearning/common/Layers.py
+++ deeplearning/common/Layers.py
@@ -1,5 +1,5 @@
 import numpy as np
-from function import sigmoid
+from function import sigmoid, softmax, cross_entropy_error
 class Relu :
     def __init__(self) :
         self.mask = None
@@ -16,7 +16,7 @@ class Relu :
         dx = dout
         return dx
     
-class sigmoid :
+class Sigmoid :
     def __init__(self) :
         self.out = None
     
@@ -29,7 +29,7 @@ class sigmoid :
         dx = dout * (1.0 - self.out) * self.out
         return dx
     
-class affine :
+class Affine :
     def __init__(self, W, b):
         self.W = W
         self.b = b
@@ -47,4 +47,21 @@ class affine :
         self.dw = np.dot(self.x.T, dout)
         self.db = np.sum(dout, axis=0)
         
+        return dx
+    
+class SoftmaxWithLoss :
+    def __init__(self) :
+        self.loss = None
+        self.y = None
+        self.t = None
+
+    def forward(self, x, t) :
+        self.t = t
+        self.y = softmax(x)
+        self.loss = cross_entropy_error(self.y, self.t)
+    
+    def backward(self, dout = 1) :
+        batch_size = self.t.shape[0]
+        dx = (self.y - self.t) / batch_size
+
         return dx
