744d4c3935e3d133cd9d4e2f98f0d6f67a606c8b
kys <kys1479@koreatech.ac.kr>
2024-08-22 11:29:49 +0900

    Chapter05 clear

--- deeplearning/Chapter05/two_layer_net.py
+++ deeplearning/Chapter05/two_layer_net.py
@@ -5,7 +5,6 @@ from collections import OrderedDict
 sys.path.append(os.pardir)
 from common.layers import *
 from common.gradient import numerical_gradient
-from dataset.mnist import load_mnist
 
 class TwoLayerNet:
     def __init__(self, input_size, hidden_size, output_size,
@@ -80,21 +79,3 @@ class TwoLayerNet:
         grads['b2'] = self.layers['Affine2'].db
 
         return grads
-
-if __name__ == '__main__':
-    # 데이터 읽기
-    (x_train, t_train), (x_test, t_test) = \
-        load_mnist(normalize=True, one_hot_label=True)
-
-    network = TwoLayerNet(input_size=784, hidden_size=50, output_size=10)
-
-    x_batch = x_train[:3]
-    t_batch = t_train[:3]
-
-    grad_numerical = network.numerical_gradient(x_batch, t_batch)
-    grad_backprop = network.gradient(x_batch, t_batch)
-
-    # 각 가중치의 차이의 절댓값을 구한 후, 그 절댓값들의 평균을 낸다.
-    for key in grad_numerical.keys():
-        diff = np.average(np.abs(grad_backprop[key] - grad_numerical[key]))
-        print(key + ":" + str(diff))
